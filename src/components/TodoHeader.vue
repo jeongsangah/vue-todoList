9e8bc5aca0bfb1aae767c5f22a329b75f6fc9416
jeongsangah <tkddk2526@naver.com>
2019-10-08 00:22:14 +0900

    컴포넌트 등록, 스타일클래스 등록, index.js 링크 추가

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+      <h1>TODO it!</h1>
+  </header>
+</template>
+
+// scoped = 이 컴포넌트 안에서만 사용한다.
+<style scoped>
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
+</style>

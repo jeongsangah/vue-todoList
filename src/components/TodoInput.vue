58dbdad48e03690d204379274da7616cc755c2f8
jeongsangah <tkddk2526@naver.com>
2019-10-11 17:55:55 +0900

    fontasome cdn 링크 수정_회사

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,58 +1,54 @@
 <template>
-  <div class="inputBox shadow">
-      <input type="text" v-model="newTodoItem">
-      <!-- <button @click="addTodo">add</button> -->
-      <span class="addContainer" @click="addTodo">
-        <i class="fas fa-plus addBtn"></i>
-      </span>
-  </div>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" @keyup.enter="addTodo">
+        <span class="addContainer" @click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
+    </div>
 </template>
- <script src="https://use.fontawesome.com/releases/v5.11.2/js/all.js"></script>
+
 <script>
 export default {
-  data() {
-    return {
-      newTodoItem: ""
-    }
-  },
-  methods: {
-    addTodo() {
-      localStorage.setItem(this.newTodoItem, this.newTodoItem)
-      this.clearInput()
+    data: function() {
+        return {
+            newTodoItem: ""
+        }
     },
-    clearInput() {
-       this.newTodoItem = ''
+    methods: {
+        addTodo: function() {
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.clearInput();
+        },
+        clearInput: function() {
+            this.newTodoItem = '';
+        }
     }
-  }
 }
 </script>
 
 <style scoped>
-  input:focus {
-    outline: none;
-  }
-  .inputBox {
-    background: white;
-    height: 50px;
-    line-height: 50px;
-    border-radius: 5px;
-  }
-  .inputBox input {
-    border-style: none;
-    font-size: 0.9rem;
-  }
-  .addContainer {
-    float: right;
-    background: linear-gradient(to right, #6478FB #8763FB);
-    display: block;
-    width: 3rem;
-    border-radius: 0 5px 5px 0;
-  }
-  .addBtn {
-    color: white;
-    vertical-align: middle;
-  }
-  .shadow {
-   box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
- }
-</style>
+    input:focus{
+        outline: none;
+    }
+    .inputBox{
+        background: white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input{
+        border-style: none;
+        font-size: 0.9rem;
+    }
+    .addContainer{
+        float: right;
+        background: linear-gradient(to right, #6478fb, #8763fb);
+        display: block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+    }
+    .addBtn{
+        color: white;
+        vertical-align: middle;
+    }
+  </style>
